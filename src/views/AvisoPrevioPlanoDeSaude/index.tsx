d4f29b8d99f10c4f30a42f58bd7c8b8005f2161e
Uthan H <uthan.hercules12@gmail.com>
2022-10-31 20:18:24 -0300

    Hero section fix

--- src/views/AvisoPrevioPlanoDeSaude/index.tsx
+++ src/views/AvisoPrevioPlanoDeSaude/index.tsx
@@ -1,10 +1,4 @@
-import {
-  Heading,
-  Text,
-  Button,
-  UnorderedList,
-  ListItem,
-} from '@chakra-ui/react';
+import { Heading, Text, Button } from '@chakra-ui/react';
 import WhatsAppButton from '../../components/WhasAppButton';
 
 import logoVW from '../../assets/logoVW.png';
@@ -24,32 +18,7 @@ const index = () => {
         </Text>
         <Button size='lg'>Não quero pagar 92% a mais</Button>
       </article>
-      <article className='more-details'>
-        <section className='text-box'>
-          <Heading as='h3'>Entenda melhor o que está acontecendo</Heading>
-          <Text as='p'>
-            Se o beneficiário pediu para cancelar o plano, eles estão cobrando o
-            chamado aviso prévio que é o período cobrado pelos planos de saúde
-            após o pedido de cancelamento do contrato.
-          </Text>
-          <Text as='p'>
-            Se o beneficiário teve seu plano de saúde cancelado por
-            inadimplência, a justificativa é diferente. Após 60 dias de
-            inadimplemento, por lei, a operadora de plano de saúde pode
-            suspender a cobertura, porém, suspendem antes disso.
-          </Text>
-          <UnorderedList>
-            <ListItem>
-              Você pede o cancelamento 🡪 O plano de saúde suspende a cobertura 🡪
-              porém a cobrança permanece por mais 60 dias.
-            </ListItem>
-            <ListItem>
-              Você para de pagar 🡪 x dias depois o plano suspende a cobertura 🡪
-              porém a cobrança permanece até completar os 60 dias.
-            </ListItem>
-          </UnorderedList>
-        </section>
-      </article>
+      <article className='more-details'></article>
     </main>
   );
 };
